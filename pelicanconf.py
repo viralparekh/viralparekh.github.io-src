c67857599449c9d2309a1e694cabf5a405e8b27a
viral parekh <viral@live.in>
2017-04-04 23:40:03 +0530

    theme add

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,7 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME = 'themes/new-bootstrap2'
+NEWEST_FIRST_ARCHIVES = True
+
