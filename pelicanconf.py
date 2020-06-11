b1137cb41b2704fd89b4f73032884423e47dfbec
Viral Parekh <viral.parekh@victor.local>
2020-06-11 12:15:31 +0530

    remove pyc, change publish path

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 AUTHOR = u'Viral Parekh'
 SITENAME = u'Viral Parekh'
 SITEURL = 'https://viralparekh.github.io'
-OUTPUT_PATH = '/Users/viral.parekh/work/repos/published_html/'
+OUTPUT_PATH = '/Users/viral.parekh/work/repos/viralparekh.github.io/'
 STATIC_PATHS = ['images', 'pdfs']
 PATH = 'content'
 TIMEZONE = 'Europe/Paris'
@@ -28,7 +28,7 @@ NEWEST_FIRST_ARCHIVES = True
 
 #pelican-hyde specific
 PROFILE_IMAGE='me.jpg'
-BIO='Research Scholar @IIIT Hyderabad, Maker, Foodie, Pro Gujarati, Chef & Writer in progress.'
+BIO='Data-Scientist at Flipkart, Maker, Foodie.'
 SOCIAL =( ('linkedin','https://www.linkedin.com/in/vparekh1'),
 	  ('twitter', 'https://twitter.com/viralmparekh'),
           ('facebook', 'https://facebook.com/viral034'),
